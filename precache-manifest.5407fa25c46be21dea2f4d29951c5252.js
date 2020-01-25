468ecb2d37aea6a54ea02326eb091083bc883935
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-25 22:10:02 +0300

    Updates

---
+++ precache-manifest.5407fa25c46be21dea2f4d29951c5252.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188102863918084c7159998b13588a95",
+    "url": "/2048-clone/index.html"
+  },
+  {
+    "revision": "12c6a2af5e304679e4f1",
+    "url": "/2048-clone/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "0dda83e75c259bcdb78b",
+    "url": "/2048-clone/static/css/main.8f51d086.chunk.css"
+  },
+  {
+    "revision": "12c6a2af5e304679e4f1",
+    "url": "/2048-clone/static/js/2.2aa5dd76.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/2048-clone/static/js/2.2aa5dd76.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0dda83e75c259bcdb78b",
+    "url": "/2048-clone/static/js/main.1a5e2e9b.chunk.js"
+  },
+  {
+    "revision": "040ba41ac732b5cd652c",
+    "url": "/2048-clone/static/js/runtime-main.cde96adc.js"
+  }
+]);
