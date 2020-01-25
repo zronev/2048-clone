6e932da03f5185bb793b6e1bc97b5a21329243ac
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-25 22:02:16 +0300

    Added all colors for tiles

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,9 +3,10 @@ import React from 'react'
 const Cell = ({ value }) => {
   return (
     <div
-      className={`grid__cell ${value ? '' : 'grid__cell--empty'} ${
-        value > 2 ? `grid__cell--${value}` : ''
-      }`}
+      className={`grid__cell 
+      ${value ? '' : 'grid__cell--empty'} 
+      ${value > 2 && value <= 2048 ? `grid__cell--${value}` : ''}
+      ${value > 2048 ? `grid__cell--over-2048` : ''}`}
     >
       {value ? value : ''}
     </div>
