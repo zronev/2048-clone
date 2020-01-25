554446148516295ec79451fce47367f75a072ade
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-26 01:07:22 +0300

    Added Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Grid from './components/Grid'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="main">
       <Header />
       <Grid />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__para">
+        <span className="bold">HOW TO PLAY</span>: Use your{' '}
+        <span className="bold">arrow keys</span> to move the tiles. When two tiles with
+        the same number touch, they <span className="bold">merge into one!</span>
+      </p>
+
+      <hr className="footer__hr" />
+
+      <p className="footer__para">
+        This game was created by{' '}
+        <a
+          href="http://gabrielecirulli.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link bold"
+        >
+          Gabriele Cirulli
+        </a>{' '}
+        — original game{' '}
+        <a
+          href="http://git.io/2048"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="link bold"
+        >
+          website.
+        </a>{' '}
+        I've just cloned the game for fun and testing my code skills.
+      </p>
+
+      <hr className="footer__hr" />
+
+      <div className="footer__links">
+        <a
+          href="https://github.com/zronev/2048-clone"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button"
+        >
+          Repo
+        </a>
+        <a
+          href="https://github.com/zronev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="button"
+        >
+          Github profile
+        </a>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
