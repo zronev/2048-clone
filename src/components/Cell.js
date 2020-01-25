08170904af5cc67fa6e282be9aa7378508d38cac
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-25 20:04:15 +0300

    Added classes for diff values

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Cell = ({ value }) => {
-  return <div className={`grid__cell ${value ? '' : 'grid__cell--empty'}`}>{value ? value : ''}</div>
+  return (
+    <div
+      className={`grid__cell ${value ? '' : 'grid__cell--empty'} ${
+        value > 2 ? `grid__cell--${value}` : ''
+      }`}
+    >
+      {value ? value : ''}
+    </div>
+  )
 }
 
 export default Cell
