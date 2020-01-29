de5aa5ae61088b0df2fb1369118346b9c66af602
Michael Nevzorov <mvnevzorov@gmail.com>
2020-01-30 00:55:08 +0300

    Bug fixed

--- src/helpers/utility.js
+++ src/helpers/utility.js
@@ -1,3 +1,8 @@
-export const arrayClone = arr => JSON.parse(JSON.stringify(arr)) // to deep clone matrix
+export const arrayClone = arr => JSON.parse(JSON.stringify(arr)); // to deep clone matrix
 
-export const arraysEqual = (arr1, arr2) => JSON.stringify(arr1) === JSON.stringify(arr2)
+export const arraysEqual = (arr1, arr2) => {
+  const newArr1 = arr1.map(row => row.map(cell => cell.value))
+  const newArr2 = arr2.map(row => row.map(cell => cell.value))
+
+  return JSON.stringify(newArr1) === JSON.stringify(newArr2);
+};
